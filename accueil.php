526928ca39e79d8d46efe6b7e15c5fc649dc2164
Alphanie Midelet <alphanie.midelet@ac-lyon.fr>
2024-09-05 16:05:52 +0200

    Mon projet version 2

--- accueil.php
+++ accueil.php
@@ -1,4 +1,16 @@
-Bonjour <?php
-    echo $_POST["prenom"]." ".$_POST['nom'];
-    ?>
+Bonjour <?php echo $_POST["prenom"]." ".$_POST['nom']; ?>
+<?php
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    if ($_POST["login"]=="admin" && $_POST["mdp"]=="azerty") {
 
+        $extra = 'profil.html';
+        header("Location: http://$host$uri/$extra");
+        exit;
+    }else{
+        $extra = 'index.html';
+        header("Location: http://$host$uri/$extra");
+        exit;
+    }
+
+?>
